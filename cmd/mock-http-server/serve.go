b142c182a062d915fe3b055a8399b70819c9afdc
NickLarsenNZ <nick@aptiv.co.nz>
2020-07-31 22:33:34 +1200

    Ensure correct behavior as handlers are overwritten for path/method combinations.

--- cmd/mock-http-server/serve.go
+++ cmd/mock-http-server/serve.go
@@ -14,22 +14,27 @@ import (
 func newServer(config *responders.ResponderConfig, bind_addr string, bind_port int) *http.Server {
 	r := mux.NewRouter()
 
-	for _, res := range config.Responders {
-		var responder = res // https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
+	for _, res_iter := range config.Responders {
+		var responder = res_iter // https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
 		handler := func(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Hit on:", r.Method, r.URL.Path)
 
-			for k, v := range responder.Then.Headers {
-				w.Header().Set(k, v)
+			res := mockhttp.MatchResponse(r, config)
+
+			for k, values := range res.Header {
+				for _, v := range values {
+					fmt.Printf("Setting: %s: %s\n", k, v)
+					w.Header().Set(k, v)
+				}
 			}
 
-			w.WriteHeader(responder.Then.Http.Status)
+			w.WriteHeader(res.StatusCode)
 
-			res := mockhttp.MatchResponse(r, config)
 			io.Copy(w, res.Body) // https://stackoverflow.com/a/28891552
 			res.Body.Close()
 		}
 
+		// Add, or update the handler for the given path
 		r.HandleFunc(responder.When.Http.Path, handler).Methods(responder.When.Http.Method)
 	}
 
